06e88cb76410c21d53d73ba5c2f9f253a8954d28
johnstennett24 <jstennett830@gmail.com>
2022-06-27 11:44:41 -0600

    added the stack class example

---
+++ Stacks/Examples/Program.cs
@@ -0,0 +1,23 @@
+﻿using classes;
+public class Program{
+
+static void Main(string[] args){
+
+    // New instance of the stack class
+    stack stack = new stack();
+
+    // Test case 1
+    Console.WriteLine("====Test Case 1====");
+
+    // Creates the new Stack
+    Stack<string> words = stack.newStack();
+
+    // Adds a word to the stack
+    string word = ("Word");
+    stack.addWord(words, word);
+    Console.WriteLine(words);
+
+
+    Console.WriteLine("===================");
+    }
+}
